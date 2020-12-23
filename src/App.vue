3d8ea3dc611f268157c60845550bf027d441f363
noahlafferty <noah.lafferty@gmail.com>
2020-12-23 16:38:03 -0600

    basically done

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id='app'>
+    <h1>National Parks</h1>
+    <ul class='parks-container'>
+      <ParkCard v-for='(park, index) in parks' :key='index' :park='park' />
+    </ul>
+  </div>
+</template>
+
+<script>
+import ParkCard from './components/ParkCard'
+
+export default {
+  name: 'app',
+  components: { ParkCard },
+  data () {
+    return {
+      parks: null
+    }
+  },
+  mounted () {
+    fetch('https://cors-anywhere.herokuapp.com/https://storage.googleapis.com/public-images-proton/parks2.json')
+      .then(res => res.json())
+      .then(data => { 
+        this.parks = data
+        console.log(this.parks)
+      })
+  }
+}
+</script>
+
+<style lang='scss'>
+html, body {
+  margin: 0;
+  padding: 0;
+
+  font-size: 16px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  padding: 1rem;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+}
+
+a {
+  color: #42b970;
+}
+
+.parks-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+  grid-gap: 1rem;
+}
+</style>
---
+++ src/components/ParkCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <li class='park-card'>
+    <img :src='park.Thumbnail'/>
+    <h3 class='park-card__name'>{{ park.Name }}</h3>
+    <p>Location: {{ park.Location }}</p>
+    <p>Established: {{ park.Established }}</p>
+    <p>Area: {{ park.Area }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ParkCard',
+  props: ['park']
+}
+</script>
+
+<style lang='scss' scoped>
+.park-card {
+  border: 1px solid black;
+}
+</style>
