8172ffa51821b0ebe005b038e95ff02e3d74138b
Joseph Kibe <jkibe@mac.com>
2020-12-23 14:45:44 -0800

    Change address binding

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,8 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     noInfo: true,
-    overlay: true
+    overlay: true,
+    host: '0.0.0.0'
   },
   performance: {
     hints: false
